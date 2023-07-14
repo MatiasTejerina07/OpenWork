b0fd762e61012aee5931703b4800245e9ff51c38
Mat Tejerina <matiastejerina94@gmail.com>
2023-07-14 15:32:02 -0300

    form

--- components/NewEntries.tsx
+++ components/NewEntries.tsx
@@ -1,54 +1,83 @@
+import { ChangeEvent, FC, useState } from 'react'
 import { Box, Button, TextField } from '@mui/material'
-import { FC } from 'react'
 import { SaveAltOutlined, CancelRounded, AddCommentOutlined } from '@mui/icons-material';
 
 
 
 
 export const NewEntry: FC = () => {
+
+    const [isAdding, setIsAdding] = useState(false)
+    const [touched, setTouched] = useState(false)
+    const [inputValue, setInputValue] = useState('')
+
+    const onTextFieldChanged = (event: ChangeEvent<HTMLInputElement>) => {
+
+
+
+    }
+
+
+
+
     return (
-        <>
-            <Box className='flex flex-col justify-center items-center w-11/12'>
+
+        <Box className='flex flex-col justify-center items-center w-11/12'>
 
 
-                <Button
-                    className='w-11/12'
-                    startIcon={<AddCommentOutlined />}
+            {
+                isAdding ? (
 
-                    variant='text'
-                >
-                    AGREGAR TAREA
-                </Button>
-                <TextField
-                    className='w-11/12'
-                    sx={{ marginTop: 2, marginBottom: 1 }}
-                    placeholder='Nueva entrada'
-                    autoFocus
-                    multiline
-                    label='Nueva entrada'
-                    helperText='Ingrese un valor'
-                />
-                <Box className='flex justify-between w-11/12'>
-                    <Button
+                    <>
+                        <TextField
+                            className='w-11/12'
+                            sx={{ marginTop: 2, marginBottom: 1 }}
+                            placeholder='Nueva entrada'
+                            autoFocus
+                            multiline
+                            label='Nueva entrada'
+                            helperText='Ingrese un valor'
+                            value={inputValue}
+                            onChange={onTextFieldChanged}
+                        />
+                        <Box className='flex justify-between w-11/12'>
+                            <Button
+                                variant='outlined'
+                                color='error'
+                                endIcon={<CancelRounded />}
+                                onClick={() => setIsAdding(false)}
+                            >
+                                Cancelar
+                            </Button>
+                            <Button
+                                className=''
+                                variant='outlined'
+                                color='secondary'
+                                endIcon={<SaveAltOutlined />}
+                            >
+                                Guardar
+                            </Button>
 
-                        variant='outlined'
-                        color='error'
-                        endIcon={<CancelRounded />}
-                    >
-                        Cancelar
-                    </Button>
-                    <Button
-                        className=''
-                        variant='outlined'
-                        color='secondary'
-                        endIcon={<SaveAltOutlined />}
-                    >
-                        Guardar
-                    </Button>
-                </Box>
+                        </Box>
+                    </>
+
+                )
+
+                    : (
+
+                        <Button
+                            className='w-11/12'
+                            startIcon={<AddCommentOutlined />}
+                            onClick={() => setIsAdding(true)}
+                            variant='text'
+                        >
+                            AGREGAR TAREA
+                        </Button>
+
+                    )}
 
 
-            </Box>
-        </>
+        </Box>
+
     )
 }
